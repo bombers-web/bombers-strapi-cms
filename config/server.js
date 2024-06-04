3bde1a9cf6490a41f069c67de1a18cd142251571
Ivan <ivanrr308@gmail.com>
2024-06-04 00:40:40 -0500

    migrate strapi v3 to v4

--- config/server.js
+++ config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1339),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b27d1918edee51b4aba375ca418f1f04'),
-    },
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
