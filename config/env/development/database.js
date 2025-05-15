88390389bde749a9a6841afa9d0942ee579b6610
edavidson1410 <edavidson1410@gmail.com>
2025-05-15 17:09:57 -0500

    database cleanup

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,39 +1,18 @@
-// const parse = require("pg-connection-string").parse;
-// const path = require('path');
-// const config = process.env.DATABASE_URL ? parse(process.env.DATABASE_URL) : ''
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: "sqlite",
-//     connection: {
-//         filename: path.join(
-//           __dirname,
-//           "..",
-//           "..",
-//           env("DATABASE_FILENAME", ".tmp/data.db")
-//         ),
-//       },
-//       useNullAsDefault: true,
-//     },
-// });
 const parse = require("pg-connection-string").parse;
-const path = require("path");
-const { settings } = require("../../../v3/config/middleware");
-const config = parse(process.env.DATABASE_URL);
+const path = require('path');
+const config = process.env.DATABASE_URL ? parse(process.env.DATABASE_URL) : ''
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: "sqlite",
     connection: {
-      host: "127.0.0.1",
-      port: "5432",
-      database: "dbstrapi-dev",
-      user: "postgres",
-      password: "Mizzou1410?",
-      settings: {
-        forceMigration: false,
-        runMigrations: false,
-      }
+        filename: path.join(
+          __dirname,
+          "..",
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db")
+        ),
+      },
+      useNullAsDefault: true,
     },
-  },
 });
