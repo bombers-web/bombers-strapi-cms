cfadea5583d52a31ce84b8422d2e32eba9693c0c
edavidson1410 <edavidson1410@gmail.com>
2025-11-17 14:04:12 -0600

    feat: strapi upgrade to v5

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,18 +1,27 @@
-const parse = require("pg-connection-string").parse;
-const path = require('path');
-const config = process.env.DEV_DATABASE_URL ? parse(process.env.DEV_DATABASE_URL) : ''
+// // New v5 format
+// module.exports = ({ env }) => ({
+//   client: env('DATABASE_CLIENT', 'sqlite'),
+//   connection: {
+//     filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//     // PostgreSQL config for production
+//     host: env('DATABASE_HOST'),
+//     port: env.int('DATABASE_PORT'),
+//     database: env('DATABASE_NAME'),
+//     user: env('DATABASE_USERNAME'),
+//     password: env('DATABASE_PASSWORD'),
+//     ssl: env.bool('DATABASE_SSL', false) && {
+//       rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+//     },
+//   },
+// });
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "sqlite",
+    client: 'sqlite',
     connection: {
-        filename: path.join(
-          __dirname,
-          "..",
-          "..",
-          env("DATABASE_FILENAME", ".tmp/data.db")
-        ),
-      },
-      useNullAsDefault: true,
+      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
     },
+    useNullAsDefault: true,
+    debug: false,
+  },
 });
