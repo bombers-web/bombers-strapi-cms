e9af85de3ae8a40d46763279efe2c47871c37cda
edavidson1410 <edavidson1410@gmail.com>
2026-05-08 20:09:53 -0500

    feat: ci/cd updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,25 @@
 module.exports = {
-    apps: [
-      {
-        name: 'strapi-app',
-        cwd: '.',
-        script: 'npm',
-        args: 'run start',
-        interpreter: '/home/ubuntu/.nvm/versions/node/v20.19.6/bin/node',
-        env: {
-          NODE_ENV: 'production'
-        },
-        output: './logs/production.out.log',
-        error: './logs/production.err.log',
-        log_date_format: 'YYYY-MM-DD HH:mm:ss',
-        max_memory_restart: '800M',
-        max_restarts: 10,
-        min_uptime: '5s',
-        restart_delay: 4000,
-        kill_timeout: 3000,
-        exec_mode: 'fork',
-        instances: 1,
-        autorestart: true,
-        watch: false,
+  apps: [
+    {
+      name: 'strapi-production',
+      cwd: '.',
+      script: 'npm',
+      args: 'run start',
+      env: {
+        NODE_ENV: 'production',
       },
-    ],
-  };
+      output: './logs/production.out.log',
+      error: './logs/production.err.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      max_memory_restart: '800M',
+      max_restarts: 10,
+      min_uptime: '5s',
+      restart_delay: 4000,
+      kill_timeout: 3000,
+      exec_mode: 'fork',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+    },
+  ],
+};
